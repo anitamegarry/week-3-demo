fb82a877df02137b2309a705219257b541c9abaf
Dedekind561 <mitchell.lloyd154@gmail.com>
2025-03-18 12:13:01 +0000

    setup test script for App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,26 @@
-import {describe, test,} from 'vitest';
-import { render, screen, waitForElementToBeRemoved } from '@testing-library/react'
+import { describe, beforeAll, afterEach, afterAll, test } from "vitest";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { server } from "./mocks/node.ts";
 
-import App from './App';
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
-describe('App', () => {
+import App from "./App";
 
-    test('loads data from an API on initial render',async() => {
-        // render the App
-        render(<App />)
-        
-        // wait for loading element to be removed...
-        await waitForElementToBeRemoved(screen.queryByText(/loading.../));
-        
-        // verify we can see the data from one object
-        screen.getByText('Winter is Coming')
-    })
+describe("App", () => {
+  test("loads data from an API on initial render", async () => {
+    // render the App
+    render(<App />);
+
+    // wait for loading element to be removed...
+    await waitForElementToBeRemoved(screen.queryByText(/loading.../));
+
+    // verify we can see the data from one object
+    screen.getByText("Winter is Coming");
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,41 @@
-import { useEffect, useState } from 'react';
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-
-
-  const [episodes,setEpisodes] = useState([]);
-  const [isLoading,setIsLoading] = useState(false);
+  const [episodes, setEpisodes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function fetchEpisodes() {
       setIsLoading(true);
-      const url  = "https://api.tvmaze.com/shows/82/episodes";
-  
+      const url = "https://api.tvmaze.com/shows/82/episodes";
+
       const response = await fetch(url);
       const body = await response.json();
 
       setEpisodes(body);
       setIsLoading(false);
-      
-  }
+    }
 
-  fetchEpisodes();
-  },
-  [])
+    fetchEpisodes();
+  }, []);
 
   // isLoading
   // episodes
-  if(isLoading) {
-    return <p>loading...</p>
+  if (isLoading) {
+    return <p>loading...</p>;
   }
   return (
     <>
-    {episodes.map(episode => {
-      return <section key={episode.id}>
-          <h3>{episode.name}</h3>
-        </section>
-    })}
+      {episodes.map((episode) => {
+        return (
+          <section key={episode.id}>
+            <h3>{episode.name}</h3>
+          </section>
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
