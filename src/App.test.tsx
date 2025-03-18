bc8b211dc8681caf656a17e0ba82fca0fca3c9a5
Dedekind561 <mitchell.lloyd154@gmail.com>
2025-03-18 10:19:28 +0000

    setup first test for data fetch

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,21 +1,18 @@
-import {describe, test, expect,} from 'vitest';
+import {describe, test,} from 'vitest';
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react'
-import Random from './components/Random';
 
+import App from './App';
 
 describe('App', () => {
-    test('check tests are working...',() => {
-        expect(1 + 2).toBe(3);
-    })
-    test('check Random component',() => {
-        render(<Random />)
-        screen.debug() // Logs the DOM structure
-        const element = screen.getByText('Random component heading here...')
-        expect(element).toBeInTheDocument()
-    });
-    test('loads data from an API on initial render',() => {
-        // render the App,
-        // load some data
-        // check we've rendered the correct data
+
+    test('loads data from an API on initial render',async() => {
+        // render the App
+        render(<App />)
+        
+        // wait for loading element to be removed...
+        await waitForElementToBeRemoved(screen.queryByText(/loading.../));
+        
+        // verify we can see the data from one object
+        screen.getByText('Winter is Coming')
     })
 });
