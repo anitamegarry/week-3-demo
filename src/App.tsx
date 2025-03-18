ddf51d1aaf70f589ff49f45252e4b49f7e13014e
Dedekind561 <mitchell.lloyd154@gmail.com>
2025-03-18 10:44:09 +0000

    implement initial data fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
 
 
+  const [episodes,setEpisodes] = useState([]);
+  const [isLoading,setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchEpisodes() {
+      setIsLoading(true);
+      const url  = "https://api.tvmaze.com/shows/82/episodes";
+  
+      const response = await fetch(url);
+      const body = await response.json();
+
+      setEpisodes(body);
+      setIsLoading(false);
+      
+  }
+
+  fetchEpisodes();
+  },
+  [])
+
+  // isLoading
+  // episodes
+  if(isLoading) {
+    return <p>loading...</p>
+  }
   return (
     <>
-    <p>loading...</p>
+    {episodes.map(episode => {
+      return <section key={episode.id}>
+          <h3>{episode.name}</h3>
+        </section>
+    })}
     </>
   )
 }
