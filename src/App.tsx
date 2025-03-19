fbad69ff997acd2aa0ad39ba349d5cffe50ba9e9
Dedekind561 <mitchell.lloyd154@gmail.com>
2025-03-19 10:26:21 +0000

    move state to NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import NavBar from "./components/NavBar";
 function App() {
   const [episodes, setEpisodes] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [shows, setShows] = useState([]);
 
   useEffect(() => {
     async function fetchEpisodes() {
@@ -22,24 +21,7 @@ function App() {
     fetchEpisodes();
   }, []);
 
-  useEffect(() => {
-    async function fetchShows() {
-      const url = "https://api.tvmaze.com/shows";
-
-      const response = await fetch(url);
-      const body = await response.json();
-      console.log(body, "<----- logging all the shows from the API");
-      setShows(body);
-    }
-
-    fetchShows();
-  }, []);
-
-  // isLoading...
-  // episodes...
-  console.log(episodes);
   if (isLoading) {
-    console.log({ isLoading });
     return <p>loading...</p>;
   }
   return (
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
+
 export default function NavBar() {
+  const [shows, setShows] = useState([]);
+
+  useEffect(() => {
+    async function fetchShows() {
+      const url = "https://api.tvmaze.com/shows";
+
+      const response = await fetch(url);
+      const body = await response.json();
+
+      setShows(body);
+    }
+
+    fetchShows();
+  }, []);
+
   return (
     <>
-      <select name="select-shows" id="select-shows">
-        <option value=""></option>
-        <option value=""></option>
-        <option value=""></option>
+      <select title="select-shows" name="select-shows" id="select-shows">
+        {shows.map((show) => {
+          return <option value={show.name}>{show.name}</option>;
+        })}
       </select>
     </>
   );
